09e7db8ec8aa9a302020f9c946a3c7244b5e0233
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-29 13:03:27 +0500

    Task1

--- Tyuiu.MamatkulovFO.Sprint6.Task1.V7/Program.cs
+++ Tyuiu.MamatkulovFO.Sprint6.Task1.V7/Program.cs
@@ -5,7 +5,7 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace Tyuiu.MamatkulovFO.Sprint6.Task1.V7
 {
-    class Program
+    class P
     {
         public static object DockStyle { get; private set; }
 
@@ -13,12 +13,21 @@ namespace Tyuiu.MamatkulovFO.Sprint6.Task1.V7
         static void Main()
         {
             var f = new Form();
-            var t = new TextBox { Multiline = true, Dock = DockStyle};
+            var t = new TextBox { Multiline = true, Dock = DockStyle };
             var b = new Button { Text = "Hisoblash", Dock = DockStyle };
 
-            b.Click += (_, __) => t.Text = new DataService().GetMassFunction(-5, 5);
+            b.Click += (_, __) =>
+            {
+                var r = new DataService().GetMassFunction(-5, 5);
+                for (int i = -5; i <= 5; i++)
+                {
+                    double val = r[i + 5];
+                    string formatted = val.ToString("F2").TrimEnd('0').TrimEnd(',');
+                    t.AppendText($"F({i}) = {formatted}\n");
+                }
+            };
 
-            
+           
         }
     }
 }
