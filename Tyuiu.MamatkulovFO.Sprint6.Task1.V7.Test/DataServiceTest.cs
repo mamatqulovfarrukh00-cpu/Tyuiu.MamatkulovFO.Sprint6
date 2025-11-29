173234a726fae460598815f90c381ee1cdb6fd26
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-29 11:40:05 +0500

    Task1

--- Tyuiu.MamatkulovFO.Sprint6.Task1.V7.Test/DataServiceTest.cs
+++ Tyuiu.MamatkulovFO.Sprint6.Task1.V7.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.MamatkulovFO.Sprint6.Task1.V7.Test
             string result = ds.GetMassFunction(-5, 5);
 
           
-            string expected = "[7.76, 7.36, 7.26, 7.90, 15.88, 2.00, 0.43, 5.63, 6.49, 6.49, 6.32]";
+            string expected = "[7.76, 7.36, 7.26, 7.9, 15.88, 2.0, 0.43, 5.63, 6.49, 6.49, 6.32]";
 
             Assert.AreEqual(expected, result);
         }
