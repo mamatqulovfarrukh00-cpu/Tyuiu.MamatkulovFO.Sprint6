f9b102a44b2b6d89d2fdbd7c58f7eb6b7572af2d
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-30 10:35:13 +0500

    Task4

---
+++ Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Lib;
+namespace Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Test;
+
+public class DataServiceTest
+{
+    public static void Run()
+    {
+        var service = new DataService();
+        double[] values = service.GetMassFunction(-5, 5);
+
+        Console.WriteLine("x\tF(x)");
+        for (int i = 0; i < values.Length; i++)
+        {
+            int x = -5 + i;
+            Console.WriteLine($"{x}\t{values[i]:F2}");
+        }
+    }
+}
---
+++ Tyuiu.MamatkulovFO.Sprint6.Task4.V10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Lib;
+namespace Tyuiu.MamatkulovFO.Sprint6.Task4.V10;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataServiceTest.Run(); 
+
+        Console.WriteLine("\nНажмите любую кнопка.");
+        Console.ReadKey();
+    }
+}
