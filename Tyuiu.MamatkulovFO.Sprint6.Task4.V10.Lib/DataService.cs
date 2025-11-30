d116d98bab450550de83cfd0086bad0c2232178f
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-11-30 10:39:01 +0500

    Task4

--- Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Lib/DataService.cs
@@ -8,23 +8,13 @@ namespace Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Lib
             var results = new List<double>();
             for (int x = startValue; x <= stopValue; x++)
             {
-                double result = CalculateF(x);
-                results.Add(result);
+                
+               
             }
             return results.ToArray();
         }
 
-        private double CalculateF(int x)
-        {
-            double denominator = Math.Sin(x) + 3;
-            if (Math.Abs(denominator) < 1e-10) 
-                return 0;
-
-            double numerator = 5 * x + 2.5;
-            double fraction = numerator / denominator;
-            double cosTerm = Math.Cos(x);
-            return fraction + 2 * x + cosTerm;
-        }
+       
     }
 }
 
--- Tyuiu.MamatkulovFO.Sprint6.Task4.V10/DataServiceTest.cs
+++ Tyuiu.MamatkulovFO.Sprint6.Task4.V10/DataServiceTest.cs
@@ -1,11 +1,4 @@
 ﻿
-namespace Tyuiu.MamatkulovFO.Sprint6.Task4.V10.Lib
-{
-    internal class DataServiceTest
-    {
-        internal static void Run()
-        {
-            throw new NotImplementedException();
         }
     }
 }
