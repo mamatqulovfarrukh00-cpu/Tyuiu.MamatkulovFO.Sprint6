010b1b7b72d4ac12d80471a309ba270e46e8a606
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-12-05 17:25:11 +0500

    Task6

--- Tyuiu.MamatkulovFO.Sprint6.Task6.V16.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint6.Task6.V16.Lib/DataService.cs
@@ -3,17 +3,22 @@ namespace Tyuiu.MamatkulovFO.Sprint6.Task6.V16.Lib;
 
 public class DataService : ISprint6Task6V16
 {
-    public string CollectTextFromFile(string path)
-    {
-        if (!File.Exists(path))
-            throw new FileNotFoundException("Файл не найден!", path);
+    
+    
+        public string CollectTextFromFile(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Файл не найден!", path);
 
-        var text = File.ReadAllText(path);
-        var words = text.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            var text = File.ReadAllText(path);
+            var lines = text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-       
-        var resultWords = words.Where(word => word.Contains('b') || word.Contains('B')).ToArray();
+            // Faqat birinchi satrni olib, uning ichidagi "b" harfi bor so'zlarini ajratamiz
+            var firstLine = lines[0]; // Birinchi satr
+            var words = firstLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-        return string.Join(" ", resultWords);
-    }
+            var resultWords = words.Where(word => word.Contains('b') || word.Contains('B')).ToArray();
+
+            return string.Join(" ", resultWords);
+        }
 }
