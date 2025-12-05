c89604aa3acc98c016a62888f6d3c7c338d7f03e
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-12-05 17:26:55 +0500

    Task6

--- Tyuiu.MamatkulovFO.Sprint6.Task6.V16.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint6.Task6.V16.Lib/DataService.cs
@@ -3,22 +3,22 @@ namespace Tyuiu.MamatkulovFO.Sprint6.Task6.V16.Lib;
 
 public class DataService : ISprint6Task6V16
 {
-    
-    
-        public string CollectTextFromFile(string path)
-        {
-            if (!File.Exists(path))
-                throw new FileNotFoundException("Файл не найден!", path);
 
-            var text = File.ReadAllText(path);
-            var lines = text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-            // Faqat birinchi satrni olib, uning ichidagi "b" harfi bor so'zlarini ajratamiz
-            var firstLine = lines[0]; // Birinchi satr
-            var words = firstLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    public string CollectTextFromFile(string path)
+    {
+        if (!File.Exists(path))
+            throw new FileNotFoundException("Файл не найден!", path);
 
-            var resultWords = words.Where(word => word.Contains('b') || word.Contains('B')).ToArray();
+        var text = File.ReadAllText(path);
+        var lines = text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
 
-            return string.Join(" ", resultWords);
-        }
+        // Faqat birinchi satrni olib, uning ichidagi "b" harfi bor so'zlarini ajratamiz
+        var firstLine = lines[0]; // Birinchi satr
+        var words = firstLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        var resultWords = words.Where(word => word.Contains('b') || word.Contains('B')).ToArray();
+
+        return string.Join(" ", resultWords); // Barcha "b" harfi bor so'zlar
+    }
 }
